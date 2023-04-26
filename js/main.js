a9eaa9322d44570abdd8502840500f90b5b330c5
Igor Nesterovich <igornesterovich2000@gmail.com>
2023-04-26 16:31:19 +0300

    Added tabs

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const burgerMenu = document.querySelector('.burger__menu');
 const headerMenu = document.querySelector('.header__wrap');
 const headerContent = document.querySelector('.header__content');
 
-burgerMenu.addEventListener('click', function () {
+burgerMenu.addEventListener('click', function() {
     burgerMenu.classList.toggle('active');
     headerMenu.classList.toggle('active');
     document.body.classList.toggle('lock');
@@ -13,3 +13,32 @@ burgerMenu.addEventListener('click', function () {
         headerContent.style.backgroundColor = 'rgba(0, 0, 0, 0.3)';
     }
 });
+
+const tabBtns = document.querySelectorAll('.tab__nav-btn');
+const tabItems = document.querySelectorAll('.tab__item');
+
+tabBtns.forEach(function(item) {
+    item.addEventListener('click', function() {
+        let currentBtn = item;
+        let tabId = currentBtn.getAttribute("data-tab");
+        let currentTab = document.querySelector(tabId);
+        let deskId = currentBtn.getAttribute("data-desk");
+        let currentDesk = document.querySelector(deskId);
+
+        if(!currentBtn.classList.contains('active')) {
+            tabBtns.forEach(function(item) {
+                item.classList.remove('active');
+            });
+    
+            tabItems.forEach(function(item) {
+                item.classList.remove('active');
+            });
+    
+            currentBtn.classList.add('active');
+            currentTab.classList.add('active');
+            currentDesk.classList.add('active');
+        }
+    });
+});
+
+document.querySelector('.tab__nav-btn').click();
